89dfc647951b153cd570a74662db1c68372d9b3d
davidbencosme <94316267+davidbencosme@users.noreply.github.com>
2023-09-23 23:56:20 -0400

    Implement infinite queries

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient, { FetchResponse } from "../services/api-client";
+//import platforms from "../data/platforms";
 
 
 const apiClient = new APIClient<Platform>('/platforms/lists/parents');
